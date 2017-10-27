ccc119b046dff0b8588e8223b4fc6e073ab2d265
e135724 <e135724@ie.u-ryukyu.ac.jp>
2017-10-27 13:51:22 +0900

    Initial Commit

---
+++ cpp_loader/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cpp_loader
+//
+//  Created by TATSUYA SHIGETOMI on 2017/10/27.
+//  Copyright © 2017年 shige. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
